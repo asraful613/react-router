f3810ca1151b41334acfb70e6149af482a78d241
asraful613 <atifasraful530@gmail.com>
2024-03-20 15:54:41 +0000

    complete all section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div>
-            <h2>Navbar</h2>
+            
             <nav>
                
                 <NavLink to="/">Home</NavLink>
